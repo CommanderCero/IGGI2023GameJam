8645bebba1ef273f1ba59a1e09a31bfa92fbdbba
Dominik Jeurissen <10519507+CommanderCero@users.noreply.github.com>
2023-01-13 12:09:31 +0000

    Add multiple background variants

--- CombatGame/Assets/Scripts/BackgroundMusicHandler.cs
+++ CombatGame/Assets/Scripts/BackgroundMusicHandler.cs
@@ -1,12 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BackgroundMusicHandler : MonoBehaviour
 {
     public static BackgroundMusicHandler Instance;
 
     public AudioSource BackgroundMusic;
+    public List<AudioClip> BackgroundVariants;
+
+    private Dictionary<string, int> LevelTrackAssignment = new Dictionary<string, int>
+    {
+        {"level1", 0},
+        {"level2", 1},
+        {"level3", 1},
+        {"level4", 2},
+        {"level5", 3},
+        {"level6", 4},
+        {"level7", 4},
+    };
+
     private void Awake()
     {
         if(Instance != null)
@@ -15,16 +29,29 @@ public class BackgroundMusicHandler : MonoBehaviour
             return;
         }
 
+        SceneManager.sceneLoaded += SceneManager_sceneLoaded;
+
         Instance = this;
         DontDestroyOnLoad(transform.gameObject);
-        PlayMusic();
+        PlayTrack(0);
     }
 
-    public void PlayMusic()
+    private void SceneManager_sceneLoaded(Scene arg0, LoadSceneMode arg1)
     {
-        if (BackgroundMusic.isPlaying)
-            return;
-        BackgroundMusic.Play();
+        int trackId = LevelTrackAssignment.GetValueOrDefault(arg0.name, 0);
+        PlayTrack(trackId);
+        Debug.Log($"Playing track {trackId}");
+    }
+
+    public void PlayTrack(int index)
+    {
+        var currentTime = BackgroundMusic.time;
+        BackgroundMusic.clip = BackgroundVariants[index];
+        BackgroundMusic.time = currentTime;
+        if (!BackgroundMusic.isPlaying)
+        {
+            BackgroundMusic.Play();
+        }
     }
 
     public void StopMusic()
