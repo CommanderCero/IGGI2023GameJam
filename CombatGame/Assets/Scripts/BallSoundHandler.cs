ab49228492db1224f6b5679365ed7877c6b2f98d
Dominik Jeurissen <10519507+CommanderCero@users.noreply.github.com>
2023-01-13 10:02:01 +0000

    Add rolling sound

--- CombatGame/Assets/Scripts/BallSoundHandler.cs
+++ CombatGame/Assets/Scripts/BallSoundHandler.cs
@@ -5,21 +5,19 @@ using UnityEngine;
 
 public class BallSoundHandler : MonoBehaviour
 {
+    [Header("Collision")]
     public AudioSource CollisionSfx;
     public float MinimumSpeed = 1.0f;
     public float MaximumSpeed = 10.0f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("Rolling")]
+    public AudioSource RollingSfx;
+    public float FadeOutTime = 0.1f;
+    public float RollingMinimumVolume = 0.1f;
+    public float RollingMinimumSpeed = 1.0f;
+    public float RollingMaximumSpeed = 10.0f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    private Coroutine fadeOutCoroutine;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -28,11 +26,54 @@ public class BallSoundHandler : MonoBehaviour
             return;
 
         float speed = collision.relativeVelocity.magnitude;
-        if (collision.relativeVelocity.magnitude > MinimumSpeed)
+        if (speed > MinimumSpeed)
         {
             float volume = (speed - MinimumSpeed) / (MaximumSpeed - MinimumSpeed);
             volume = Mathf.Clamp01(volume);
             CollisionSfx.PlayOneShot(CollisionSfx.clip, volume);
         }
+
+        if(speed > RollingMinimumSpeed)
+        {
+            if(fadeOutCoroutine != null)
+                StopCoroutine(fadeOutCoroutine);
+            RollingSfx.Play();
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        // Hardcoded check for colliding with the camera edges
+        if (collision.gameObject.GetComponent<Camera>() != null)
+            return;
+
+        float speed = collision.relativeVelocity.magnitude;
+        float volume = RollingMinimumVolume;
+        volume += (1f - volume) * (speed - RollingMinimumSpeed) / (RollingMaximumSpeed - RollingMinimumSpeed);
+        volume = Mathf.Clamp01(volume);
+        RollingSfx.volume = volume;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        // Hardcoded check for colliding with the camera edges
+        if (collision.gameObject.GetComponent<Camera>() != null)
+            return;
+
+       fadeOutCoroutine = StartCoroutine(FadeOutRolling());
+    }
+
+    private IEnumerator FadeOutRolling()
+    {
+        float startVolume = RollingSfx.volume;
+        float time = FadeOutTime;
+        while(time > 0)
+        {
+            RollingSfx.volume = startVolume * (time / FadeOutTime);
+            time -= Time.deltaTime;
+            yield return null;
+        }
+
+        RollingSfx.Stop();
     }
 }
